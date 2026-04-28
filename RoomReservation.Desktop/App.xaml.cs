8328eb353fcf38bb0d8e24e40bf9423d40cc0f38
moritomori <moritomori@gmail.com>
2026-04-28 23:06:32 +0200

    fix wrong version wpf

--- RoomReservation.Desktop/App.xaml.cs
+++ RoomReservation.Desktop/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace RoomReservation.Desktop
@@ -14,4 +10,5 @@ namespace RoomReservation.Desktop
 	public partial class App : Application
 	{
 	}
+
 }
