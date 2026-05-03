b6aa7df3224acf6b0a43987cf3acd46d95bdbabf
moritomori <moritomori@gmail.com>
2026-05-03 15:11:15 +0200

    add rooms overview

---
+++ RoomReservation.Web/Controllers/RoomsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoomReservation.Data.Repositories;
+
+namespace RoomReservation.Web.Controllers
+{
+	public class RoomsController : Controller
+	{
+		private readonly RoomRepository _roomRepository;
+
+		public RoomsController(RoomRepository roomRepository)
+		{
+			_roomRepository = roomRepository;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			int? userId = HttpContext.Session.GetInt32("UserId");
+
+			if (userId == null)
+			{
+				return RedirectToAction("Login", "Account");
+			}
+
+			var rooms = await _roomRepository.GetAllAsync();
+			return View(rooms);
+		}
+	}
+}
