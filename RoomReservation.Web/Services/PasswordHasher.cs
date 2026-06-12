6489dea14c8bad5dba51611c1352295434de3f61
moritomori <moritomori@gmail.com>
2026-06-12 20:33:33 +0200

    security: use BCrypt password hashing

--- RoomReservation.Web/Services/PasswordHasher.cs
+++ RoomReservation.Web/Services/PasswordHasher.cs
@@ -1,24 +1,15 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-
-namespace RoomReservation.Web.Services
+﻿namespace RoomReservation.Web.Services
 {
-	public class PasswordHasher
-	{
-		public string Hash(string password)
-		{
-			using var sha256 = SHA256.Create();
+    public class PasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
 
-			byte[] bytes = Encoding.UTF8.GetBytes(password);
-			byte[] hash = sha256.ComputeHash(bytes);
-
-			return Convert.ToBase64String(hash);
-		}
-
-		public bool Verify(string password, string passwordHash)
-		{
-			string hash = Hash(password);
-			return hash == passwordHash;
-		}
-	}
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
 }
